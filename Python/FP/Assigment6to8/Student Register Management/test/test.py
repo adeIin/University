dd910f876bc6489236a2269a36409c6ebf5460db
adeIin <62558192+adeIin@users.noreply.github.com>
2020-03-23 18:19:23 +0200

    Delete test.py

--- Python/FP/Assigment6to8/Student Register Management/test/test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from Repository import StudentRepository
-
-class TestRepo(unittest.TestCase):
-    def testAdd(self):
-        self.assertEqual(StudentRepository.addTwoNumbers(self,4,7), 11)
